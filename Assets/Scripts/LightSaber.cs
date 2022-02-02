81f4a584e0f3fa00a3a8233b215e48ba404f4237
rrbeech <autumnartist@gmail.com>
2022-02-02 16:13:21 -0500

    Pretty Much Done

--- Assets/Scripts/LightSaber.cs
+++ Assets/Scripts/LightSaber.cs
@@ -10,11 +10,14 @@ public class LightSaber : MonoBehaviour
     public AudioClip hum;
     public AudioClip powerUp;
     public AudioClip powerDown;
+    public AudioClip collisionSound;
+    private bool LightSaberON = false;
 
     public void turnOnLightSaber()
     {
         LightSaberAnimator.SetTrigger("Turn Light Saber ON");
         LightSaberAnimator.ResetTrigger("Turn Light Saber OFF"); // Reset trigger
+        LightSaberON = true;
 
         audioSource.PlayOneShot(hum); //play the steady hum
         audioSourceAux.PlayOneShot(powerUp); //play the powerup
@@ -22,13 +25,23 @@ public class LightSaber : MonoBehaviour
 
     public void turnOFFLightSaber()
     {
-        LightSaberAnimator.SetTrigger("Turn Light Saber OFF");
-        LightSaberAnimator.ResetTrigger("Turn Light Saber ON"); //Reset trigger
+        if (LightSaberON) // This 'IF' is not redundent. It fixes the situation
+                        // where the saber is dropped before the blade is turned off
+                        // If the blade is already off we don't want to play the powerDown when we drop the saber.
+        {
+            LightSaberAnimator.SetTrigger("Turn Light Saber OFF");
+            LightSaberAnimator.ResetTrigger("Turn Light Saber ON"); //Reset trigger
+            LightSaberON = false;
 
-        audioSource.Stop();//turn off hum
-        audioSourceAux.PlayOneShot(powerDown);
-    
-      
+            audioSource.Stop();//turn off hum
+            audioSourceAux.PlayOneShot(powerDown);
+        }
+        
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (LightSaberON) //play collision sound only if blade turned on
+            audioSourceAux.PlayOneShot(collisionSound);
+    }
 }
